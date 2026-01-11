41e262fe18c6e3c6586c4c0e75c11b90e6b30128
shakibalfurkan <shakibalfurkan@gmail.com>
2026-01-11 22:36:35 +0600

    database connection method added

---
+++ src/app/config/index.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+
+  mongodb_url: process.env.MONGODB_URL,
+  bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
+
+  jwt_access_token_secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+  jwt_access_token_expires_in: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
+
+  jwt_refresh_token_secret: process.env.JWT_REFRESH_TOKEN_SECRET,
+  jwt_refresh_token_expires_in: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN,
+
+  jwt_reset_token_secret: process.env.JWT_RESET_TOKEN_SECRET,
+  jwt_reset_token_expires_in: process.env.JWT_RESET_TOKEN_EXPIRES_IN,
+
+  client_url: process.env.CLIENT_URL,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import { createApp } from "./app.js";
+import config from "./app/config/index.js";
 // import config from "./app/config/index.js";
-// import connectToMongoDB from "./app/database/mongodb.js";
+import connectToMongoDB from "./app/database/mongodb.js";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || config.port;
 
 async function main(): Promise<void> {
   try {
     // Connect to MongoDB database
-    // await connectToMongoDB();
+    await connectToMongoDB();
     // Create app
     const app = await createApp();
 
