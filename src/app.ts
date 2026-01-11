67b8801497fc782474d22a0e077e1cc270de3357
shakibalfurkan <shakibalfurkan@gmail.com>
2026-01-11 22:17:52 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { type Request, type Response } from "express";
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+// import notFoundError from "./app/middlewares/notFoundError.js";
+// import globalErrorHandler from "./app/middlewares/globalErrorHandler.js";
+// import globalRouter from "./app/routes/index.js";
+
+export async function createApp(): Promise<express.Express> {
+  const app = express();
+
+  // Middleware setup
+  app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      credentials: true,
+    })
+  );
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+
+  app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+      success: true,
+      message: "Welcome to the BookWorm API service!",
+    });
+  });
+
+  //   app.use("/api/v1", globalRouter);
+
+  //   app.use(globalErrorHandler);
+  //   app.use(notFoundError);
+
+  return app;
+}
